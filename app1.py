2b33de04066d57f7b72c6452ebd7b1cfb932fffb
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-04-15 13:50:26 +0200

    interactive dictionary is ready

---
+++ app1.py
@@ -0,0 +1,29 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word,data.keys())) > 0:
+        yn = input("Did you mean %s instead? [y or n]" % get_close_matches(word, data.keys())[0]).lower()
+        if yn == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "n":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("enter a word: ")
+
+ouput = translate(word)
+
+if type(ouput) == list:
+    for item in ouput:
+        print(item)
+else:
+    print(ouput)
