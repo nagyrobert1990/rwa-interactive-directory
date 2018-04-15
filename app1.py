e0639618d7efe4a7ae20d66b7ea20e6ffe837235
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-04-15 14:31:24 +0200

    interactive dictionary is optimized for special cases

--- app1.py
+++ app1.py
@@ -7,6 +7,10 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word,data.keys())) > 0:
         yn = input("Did you mean %s instead? [y or n]" % get_close_matches(word, data.keys())[0]).lower()
         if yn == "y":
